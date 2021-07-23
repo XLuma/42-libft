894b239f626d7f42b6685615f19c9986b6c61894
Louis-gabriel Laplante <llaplant@c2r2p2.42quebec.com>
2021-07-23 08:52:28 -0400

    added new function ft_strchk

---
+++ ft_strchk.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchk.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llaplant <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/20 13:29:38 by llaplant          #+#    #+#             */
+/*   Updated: 2021/07/23 08:52:07 by llaplant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*ft_strchk: function to verify if
+ * there are any other characters in
+ * a string other than the characters
+ * in the specified charset.
+ * returns zero if only chars from the
+ * charset are present, returns 1 otherwise*/
+
+#include "libft.h"
+int	ft_strchk(char *str, char *charset)
+{
+	int	i;
+	int	j;
+	int	charset_len;
+
+	i = 0;
+	j = 0;
+	charset_len = ft_strlen(charset);
+	while (str[i])
+	{
+		while (charset[j])
+		{
+			if (str[i] == charset[j])
+				;
+			else if (j == charset_len)
+				return (1);
+			else
+				j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (0);
+}
+
+
